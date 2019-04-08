ce0d39abd06303db3a00313654d7a108c3685bbd
alok <alok.swain@magnontbwa.com>
2019-04-08 13:50:08 +0530

    change db to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_04_08_060920) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
